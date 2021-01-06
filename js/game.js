0be79459ee3de3885f9a055231a5218382ade1e6
Victor Nastasa <victornast@gmail.com>
2021-01-06 11:23:12 +0100

    add cylinder fo the sea

--- js/game.js
+++ js/game.js
@@ -1,3 +1,46 @@
+// Creating Objects
+
+// Cylinder for the Sea
+
+Sea = function () {
+  // create the geometry (shape) of the cylinder;
+  // the parameters are:
+  // radius top, radius bottom, height, number of segments on the radius, number of segments vertically
+  let geom = new THREE.CylinderGeometry(600, 600, 800, 40, 10);
+
+  // rotate the geometry on the x axis
+  geom.applyMatrix(new THREE.Matrix4().makeRotationX(-Math.PI / 2));
+
+  // create the material
+  let mat = new THREE.MeshPhongMaterial({
+    color: Colors.blue,
+    transparent: true,
+    opacity: 0.6,
+    shading: THREE.FlatShading
+  });
+
+  // To create an object in Three.js, we have to create a mesh
+  // which is a combination of a geometry and some material
+  this.mesh = new THREE.Mesh(geom, mat);
+
+  // Allow the sea to receive shadows
+  this.mesh.receiveShadow = true;
+};
+
+// Instantiate the sea and add it to the scene:
+
+let sea;
+
+function createSea() {
+  sea = new Sea();
+
+  // push it a little bit at the bottom of the scene
+  sea.mesh.position.y = -600;
+
+  // add the mesh of the sea to the scene
+  scene.add(sea.mesh);
+}
+
 // The Lights
 
 let hemisphereLight, shadowLight;
