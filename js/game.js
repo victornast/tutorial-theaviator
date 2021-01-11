11572c4b5fa918722663d80eaa306c813c414ae9
Victor Nastasa <victornast@gmail.com>
2021-01-11 21:28:29 +0100

    add world spin

--- js/game.js
+++ js/game.js
@@ -343,7 +343,15 @@ function createScene() {
 // Rendering
 
 function loop() {
+  // Rotate the propeller, the sea and the sky
+  airplane.propeller.rotation.x += 0.3;
+  sea.mesh.rotation.z += 0.005;
+  sky.mesh.rotation.z += 0.01;
+
+  // render the scene
   renderer.render(scene, camera);
+
+  // call the loop function again
   requestAnimationFrame(loop);
 }
 
